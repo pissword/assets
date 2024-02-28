9008672c0e0a4f6e4675b6836d46e1bc1ef1a431
pissword <161356884+pissword@users.noreply.github.com>
2024-02-28 19:06:33 +0000

    Update notdiscord.js

--- notdiscord.js
+++ notdiscord.js
@@ -1,6 +1,6 @@
 var visited=localStorage.getItem('visited');
 if (!visited) {
   // or, replace "pissword" with your instance name
-  alert("This is NOT Discord. pissword uses Spacebar, a FOSS reimplementation of the Discord backend. You must create a new account. \nPress \"OK\" to continue"); 
+  alert("This is NOT Discord. pissword uses Spacebar, a FOSS reimplementation of the Discord backend. You must create a new account. \n\nPress \"OK\" to continue"); 
   localStorage.setItem('visited', true);
 }
